8df61224333193c8982b2454d3d92db38eaddc8b
werb210 <todd.w@boreal.financial>
2025-10-03 14:46:01 -0600

    Update index.ts

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,12 +1,7 @@
-// Central API router aggregator. Mounted at /api in boot.ts
 import { Router } from "express";
 import o365 from "./integrations/office365";
 
 const router = Router();
-
-// Integrations
 router.use("/o365", o365);
-
-// Add other grouped routers here in the future.
 
 export default router;
