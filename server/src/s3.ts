4ed34c738199a2739ad92280c5637ae377d0cc80
werb210 <todd.w@boreal.financial>
2025-10-09 11:47:52 -0600

    Create s3.ts

---
+++ server/src/s3.ts
@@ -0,0 +1,11 @@
+import AWS from "aws-sdk";
+import dotenv from "dotenv";
+dotenv.config();
+
+AWS.config.update({
+  region: process.env.AWS_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+
+export const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
