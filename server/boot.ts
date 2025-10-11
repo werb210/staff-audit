8f494bf285c75bd43d7e0dbdb0e5182638b22c11
werb210 <todd.w@boreal.financial>
2025-10-11 13:37:22 -0600

    Update boot.ts

--- server/boot.ts
+++ server/boot.ts
@@ -2,7 +2,7 @@ import express from "express";
 import routes from "./routes/index.js";
 
 export default async function boot(app: express.Application) {
-  console.log("🧩 Restoring full route mount...");
+  console.log("@ Restoring full route mount...");
   app.use("/api", routes);
 
   app.get("/api/_int/state", (_req, res) => {
