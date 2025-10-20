6af43fea01b5535e3e9a489b17409569eaf788eb
werb210 <todd.w@boreal.financial>
2025-10-20 17:22:29 +0000

    Remove conflicting function file and deploy Express app

--- express/api/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res) => {
-  res.json([
-    { id: "1", stage: "New", businessName: "Sample Business", amount: 50000 },
-    { id: "2", stage: "Requires Docs", businessName: "Test Co", amount: 25000 }
-  ]);
-};
